b5403d6d4596b4e5bfbf79b04fc816ac96946d54
Alexandrey <alexandre.yucra@outlook.com>
2020-03-24 16:07:54 -0300

    finalizando as rotas e paginação/ back-end finalizado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,37 @@
 const express = require('express');
 
+const ongController = require('./controllers/ongController');
+const incidentController = require('./controllers/incidentController');
+const profileController = require('./controllers/profileController');
+const sessionController = require('./controllers/sessionController');
+
 const routes = express.Router();
 
-routes.post('/ongs', (request, response) => {
-    const data = request.body;
+//login
+routes.post('/sessions', sessionController.create); 
+/**
+ * ONG
+ */
+//listagem de ongs
+routes.get('/ongs', ongController.index);
 
-    console.log(data);
+//cadastro de ongs
+routes.post('/ongs', ongController.create);
 
-    return response.json();
-});
+//profile da ong/ serve para trazer os casos devido a ong
+routes.get('/profile', profileController.index);
+
+/**
+ * CASO
+ */
+//listando os  casos
+routes.get('/incidents', incidentController.index);
+
+//criando um novo caso
+routes.post('/incidents', incidentController.create);
+
+//deletando um caso
+routes.delete('/incidents/:id', incidentController.delete);
+
 
 module.exports = routes;
