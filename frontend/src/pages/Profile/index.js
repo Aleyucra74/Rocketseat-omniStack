7af62f77b903221bfe886dc50cddcaefbacd1239
Alexandrey <alexandre.yucra@outlook.com>
2020-03-25 18:26:26 -0300

    fazendo o delete dos casos

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -22,6 +22,20 @@ export default function Profile() {
         })
     }, [ongId]);
 
+    async function handleDeleteIncident(id) {
+        try {
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+
+            setIncidents(incidents.filter(incident => incident.id !== id));
+        } catch (err) {
+            alert('Erro ao tentar deletar o caso, tente novamente');
+        }
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -48,7 +62,7 @@ export default function Profile() {
                         <strong>VALOR:</strong>
                         <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
 
-                        <button type="button">
+                        <button onClick={() => handleDeleteIncident(incident.id)} type="button">
                             <FiTrash2 size={20} color="#A8A8b3" />
                         </button>
                     </li> 
